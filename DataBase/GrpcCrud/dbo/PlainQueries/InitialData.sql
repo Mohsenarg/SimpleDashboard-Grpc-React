418570bf93271e1a743b89ddbe9e4984dde51c01
Mohsen Araghi <araghi13@outlook.com>
2023-04-27 20:15:45 +0330

    ServiceUpdated

---
+++ DataBase/GrpcCrud/dbo/PlainQueries/InitialData.sql
@@ -0,0 +1,14 @@
+﻿INSERT INTO [dbo].[gtUser]
+           ([Name]
+           ,[LastName]
+           ,[IsFemale]
+           ,[Address]
+           ,[Email]
+           ,[Password])
+     VALUES
+           (N'example-name',
+           N'example-lastname',
+           1,
+           N'example-address',
+           N'example-email',
+           N'example-password')
--- GrpcServiceUser/Services/UserOpService.cs
+++ GrpcServiceUser/Services/UserOpService.cs
@@ -19,7 +19,7 @@ namespace GrpcServiceUser.Services
             UsrMapper = new Mapper(MapConfig);
         }
 
-        public override Task<User> UserLogin(UserLoginEntry UserEntry, ServerCallContext context)
+        public override Task<User> UserSelect(UserLoginEntry UserEntry, ServerCallContext context)
         {
             try
             {
@@ -48,11 +48,11 @@ namespace GrpcServiceUser.Services
 
 
         }
-        public override Task<ResultStat> UserSignUp(UserAdd user, ServerCallContext context)
+        public override Task<ResultStat> UserInsert(UserAdd user, ServerCallContext context)
         {
             try
             {
-                if (user.Name != null && user.LastName != null && user.Email != null && user.Password != null)
+                if (user.Name != null && user.LastName != null && user.Email != null && user.Password != null )
                 {
                     var addgtUser = new gtUser()
                     {
@@ -67,6 +67,74 @@ namespace GrpcServiceUser.Services
                     DbGrpcCrud.gtUser.Add(addgtUser);
                     DbGrpcCrud.SaveChanges();
                     return Task.FromResult(new ResultStat() { Ok = true });
+                }
+                else
+                {
+                    return Task.FromResult(new ResultStat() { Ok = false });
+                }
+            }
+            catch (Exception)
+            {
+
+                return Task.FromResult(new ResultStat() { Ok = false });
+            }
+        }
+        public override Task<ResultStat> UserUpdate(User user, ServerCallContext context)
+        {
+            try
+            {
+                long id = Convert.ToInt64(user.Id);
+
+                if (id != 0 && user.Name != null && user.LastName != null && user.Email != null && user.Password != null)
+                {
+                    var SeletedUser = DbGrpcCrud.gtUser.Where(q => q.Id == id).FirstOrDefault();
+                    if (SeletedUser != null)
+                    {
+                        SeletedUser.Name = user.Name == "" ? null : user.Name;
+                        SeletedUser.LastName = user.LastName == "" ? null : user.LastName;
+                        SeletedUser.IsFemale = user.IsFemale;
+                        SeletedUser.Address = user.Address == "" ? null : user.Address;
+                        SeletedUser.Email = user.Email == "" ? null : user.Email;
+                        SeletedUser.Password = user.Password == "" ? null : user.Password;
+                        DbGrpcCrud.SaveChanges();
+                        return Task.FromResult(new ResultStat() { Ok = true });
+                    }
+                    else
+                    {
+                        return Task.FromResult(new ResultStat() { Ok = false });
+                    }
+                }
+                else
+                {
+                    return Task.FromResult(new ResultStat() { Ok = false });
+                }
+            }
+            catch (Exception)
+            {
+
+                return Task.FromResult(new ResultStat() { Ok = false });
+            }
+
+        }
+        public override Task<ResultStat> UserDelete(UserId userId, ServerCallContext context)
+        {
+            try
+            {
+                long id = Convert.ToInt64(userId.Id);
+
+                if (id != 0 )
+                {
+                    var SeletedUser = DbGrpcCrud.gtUser.Where(q => q.Id == id).FirstOrDefault();
+                    if (SeletedUser != null)
+                    {
+                        DbGrpcCrud.gtUser.Remove(SeletedUser);
+                        DbGrpcCrud.SaveChanges();
+                        return Task.FromResult(new ResultStat() { Ok = true });
+                    }
+                    else
+                    {
+                        return Task.FromResult(new ResultStat() { Ok = false });
+                    }
                 }
                 else
                 {
